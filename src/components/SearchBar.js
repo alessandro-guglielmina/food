287143ef0213ddc0a18394f16cda9b976a17fa15
Alessandro Guglielmina <alessandro.guglielmina@gmail.com>
2024-07-21 23:19:56 +0200

    going on

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,27 +3,40 @@ import { View, Text, StyleSheet } from 'react-native';
 import {Feather} from '@expo/vector-icons'
 import { TextInput } from 'react-native-gesture-handler';
 
-const SearchBar = () => {
+const SearchBar = ({term, onTermChange, onTermSubmit}) => {
   return (
     <View style={styles.backgroundStyle}>
-        <Feather name="search" size={30}/>
-      <TextInput style={styles.inputStyle} placeholder='Search'/>
+        <Feather name="search" style={styles.iconStyle}/>
+      <TextInput
+      autoCapitalize="none"
+      autoCorrect={false}
+      style={styles.inputStyle}
+      placeholder='Search'
+      value={term}
+      onChangeText={newTerm => onTermChange(newTerm)}
+      onEndEditing={onTermSubmit}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
     backgroundStyle:{
-        backgroundColor: '#F0EEEE',
-        height: 50,
-        borderRadius: 5,
-        marginHorizontal: 15,
-        flexDirection: 'row'
+      marginTop: 10,
+      backgroundColor: '#F0EEEE',
+      height: 50,
+      borderRadius: 5,
+      marginHorizontal: 15,
+      flexDirection: 'row'
     },
     inputStyle:{
-        borderColor: 'black',
-        borderWidth: 1,
-        flex: 1
+      flex: 1,
+      fontSize: 18
+    },
+    iconStyle:{
+      fontSize: 35,
+      alignSelf: 'center',
+      marginHorizontal: 15
     }
 });
 
