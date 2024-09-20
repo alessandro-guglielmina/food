16fb832cd897bb25628fd1256084c65e98dd620e
Alessandro Guglielmina <alessandro.guglielmina@gmail.com>
2024-09-20 23:12:16 +0200

    first commit

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import {View, Text, StyleSheet, FlatList, TouchableOpacity} from 'react-native';
+import {withNavigation} from 'react-navigation';
+
 import ResultsDetail from './ResultsDetail';
 
 const ResultsList = ({title, results, navigation}) => {
+    if(!results.length) {
+        return null;
+    }
     return(
         <View style={styles.container}>
             <Text style={styles.title}>{title}</Text>
@@ -12,8 +17,9 @@ const ResultsList = ({title, results, navigation}) => {
             data={results}
             keyExtractor={(result) => result.id}
             renderItem={({item}) => {
+                // use navigation to navigate to the ResultsShow screen with {id: item.id} arguments
                 return (
-                    <TouchableOpacity onPress={() => navigation.navigate('ResultsShow')}>
+                    <TouchableOpacity onPress={() => navigation.navigate('ResultsShow', {id: item.id})}> 
                         <ResultsDetail result={item}/>
                     </TouchableOpacity>
                 );
@@ -35,4 +41,4 @@ const styles = StyleSheet.create({
     }
 });
 
-export default ResultsList;
+export default withNavigation(ResultsList);
--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,14 +1,48 @@
-import React from "react";
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState, useEffect} from "react";
+import {View, Text, StyleSheet, FlatList, Image} from 'react-native';
+import yelp from "../api/yelp";
 
-const ResultsShowScreen = () => {
+const ResultsShowScreen = ({navigation}) => { // cannot give id directly as argument, since we're using navigation
+    const [result, setResult] = useState(null); // null = default value
+    const id = navigation.getParam('id');
+
+
+    const getResult = async id => {
+        const response = await yelp.get('/${id}');
+        setResult(response.data);
+    };
+    useEffect(() => {
+        getResult(id);
+        }, []);
+
+       if(!result)
+       {
+        return null;
+       }
+
     return (
         <View>
-            <Text>results Show</Text>
+            <Text>{result.name}</Text>
+            <FlatList
+            data={result.photos}
+            keyExtractor={(photo) => photo}
+            renderItem={({item}) => {
+                return <Image style={styles.image} source={{uri: item}}/>
+            }}/>
         </View>
+        // <View>
+        //     <Text>
+        //         OK
+        //     </Text>
+        // </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    image:{
+        height: 200,
+        width: 300
+    }
+});
 
 export default ResultsShowScreen;
